6d77eb97a72003e16934df7c69a438bda8d2f4c6
Arpita Samant <samantarpita70@gmail.com>
2026-08-21 21:48:18 +0530

    updated

---
+++ Game-RPS/script.js
@@ -0,0 +1,17 @@
+let userScore = 0;
+let compScore = 0;
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+
+};
+
+const choices = document.querySelectorAll(".choice");
+choices.forEach((choice) =>{
+    choice.addEventListener("click", () =>{
+        const userChoice = choice.getAttribute("id");
+        console.log("choice was clicked", userChoice);
+        playGame(userChoice);
+
+    });
+});
